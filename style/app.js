7c847150cb81f91bdc18309f335743c419e1b3e9
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2020-10-16 19:57:21 +0200

    master

--- style/app.js
+++ style/app.js
@@ -1,4 +1,41 @@
-let nr1 = document.getElementById('nr1');
-let nr2 = document.getElementById('nr2');
+// let nr1 = document.getElementById('nr1').innerText;
+// let nr2 = document.getElementById('nr2').innerText;
+// let plus = document.getElementById('plus').innerText;
+// let answer = document.getElementById('answer').value;
+// let correct = document.getElementById('correct');
 
-console.log(nr1, nr2);
+// // let sum = Number(nr1) + Number(nr2);
+
+// let numberSum = Number(nr1) + Number(nr2);
+// let userSum = Number(answer);
+
+// console.log(numberSum);
+// console.log(userSum);
+
+// if (numberSum === userSum) {
+//     console.log('Rätt');
+// }   else {
+//     console.log('Fel');
+// }
+
+
+
+
+function correctedNumber () {
+let nr1 = document.getElementById('nr1').innerText;
+let nr2 = document.getElementById('nr2').innerText;
+let answer = document.getElementById('answer').value;
+
+let numberSum = Number(nr1) + Number(nr2);
+let userSum = Number(answer);
+
+console.log(numberSum);
+console.log(userSum);
+
+if (numberSum === userSum) {
+    console.log('Rätt');
+}   else {
+    console.log('Fel');
+}
+
+}

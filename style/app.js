545aee52231c93bf2de26bce5d7bc15863f48dc7
nicklas-holmqvist <nicklas_holmqvist@outlook.com>
2020-10-17 20:46:19 +0200

    master

--- style/app.js
+++ style/app.js
@@ -1,41 +1,44 @@
-// let nr1 = document.getElementById('nr1').innerText;
-// let nr2 = document.getElementById('nr2').innerText;
-// let plus = document.getElementById('plus').innerText;
-// let answer = document.getElementById('answer').value;
-// let correct = document.getElementById('correct');
+const correctAnswer = "Rätt!";
+const wrongAnswer = "Fel!";
 
-// // let sum = Number(nr1) + Number(nr2);
+let nr1 = document.getElementById('nr1').innerText = getRandomNumber(50);
+let nr2 = document.getElementById('nr2').innerText = getRandomNumber(50);
+let disableInput = document.getElementById('answer')
+let newBtn = document.getElementById('newBtn');
+let correctBtn = document.getElementById('correctBtn');
 
-// let numberSum = Number(nr1) + Number(nr2);
-// let userSum = Number(answer);
-
-// console.log(numberSum);
-// console.log(userSum);
-
-// if (numberSum === userSum) {
-//     console.log('Rätt');
-// }   else {
-//     console.log('Fel');
-// }
-
-
-
+function newNumber () {
+    reload = location.reload();
+    correctBtn.style.opacity = 1;
+    disableInput.readOnly = false;
+}
 
 function correctedNumber () {
-let nr1 = document.getElementById('nr1').innerText;
-let nr2 = document.getElementById('nr2').innerText;
-let answer = document.getElementById('answer').value;
+    let answer = document.getElementById('answer').value;
+    let corrected = document.getElementById('correct');
+    
+    let numberSum = Number(nr1) + Number(nr2);
+    let userSum = Number(answer);
 
-let numberSum = Number(nr1) + Number(nr2);
-let userSum = Number(answer);
-
-console.log(numberSum);
-console.log(userSum);
+    console.log(numberSum);
+    console.log(userSum);
 
 if (numberSum === userSum) {
+    correct.innerText = correctAnswer;
+    disableInput.readOnly = true;
     console.log('Rätt');
+    newBtn.style.opacity = 1;
+    correctBtn.style.opacity = 0;
+
 }   else {
+    corrected.innerText = wrongAnswer;
     console.log('Fel');
 }
-
 }
+
+function getRandomNumber(limit) {
+    const randomValue = Math.random() * limit;
+    return Math.round(randomValue);
+}
+
+
